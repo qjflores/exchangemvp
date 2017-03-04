fdb0163fc973674c1556934f516d8d7d3a47e745
Arseniy Klempner <adklempner@gmail.com>
2017-03-04 14:08:44 +0530

    First prototype

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -1,30 +1,70 @@
 pragma solidity ^0.4.2;
 
-contract Escrow {
-  address public scholar;
-  address public donar; //TODO handle multiple donars
-  address public arbiter; //TODO handle multiple arbiters
+import "./zeppelin/lifecycle/Killable.sol";
+import "./zeppelin/SafeMath.sol";
 
-  //TODO add a struct to store some other things about a scholar
-  //TODO add a struct to store some other things about a donar
+contract Escrow is Killable, SafeMath {
+  address private buyer;
+  address private seller; //TODO handle multiple sellers
 
-  function Escrow(address _scholar, address _arbiter) payable {
-    donar = msg.sender;
-    scholar = _scholar;
-    arbiter = _arbiter;
+  //amout is in wei
+  uint private amount;
+
+  /*uint private multiplier = 100;
+  uint private feePercent = 10; //10%*/
+
+  address multiSig;
+
+  //TODO add a struct to store some other things about a buyer
+  //TODO add a struct to store some other things about a owner
+
+  enum State { Open, InEscrow }
+
+  State public state;
+  uint private fee;
+
+  function Escrow(address _buyer, address _seller, address _multiSig, uint _amount, uint _fee) {
+    owner = msg.sender;
+    multiSig = _multiSig;
+    buyer = _buyer;
+    seller = _seller;
+    amount = _amount;
+    state = State.Open;
+    fee = _fee;
   }
 
-  function payoutToBuyer() payable {
-    if(msg.sender == donar || msg.sender == arbiter) {
-      if (!scholar.send(this.balance))
-        throw;
-    }
+
+  event InEscrow(uint block);
+
+  function pay() payable stateIs(State.Open) {
+    if(msg.value != (amount+fee))
+      throw;
+    //possible book keeping?
+    state = State.InEscrow;
+    InEscrow(block.number);
   }
 
-  function refundToSeller() payable {
-    if(msg.sender == scholar || msg.sender == arbiter) {
-      if (!donar.send(this.balance))
-        throw;
-    }
+  function payoutToBuyer() stateIs(State.InEscrow) {
+    if(msg.sender != seller && msg.sender != owner)
+      throw;
+
+    if(!buyer.send(amount))
+      throw;
+
+    selfdestruct(multiSig);
   }
+
+  function refundToSeller() stateIs(State.InEscrow) {
+    if(msg.sender != owner)
+      throw;
+
+    selfdestruct(seller);
+  }
+
+  modifier stateIs(State _state) {
+    if(state != _state)
+      throw;
+    _;
+  }
+
 }
